d6c330d6a5e22a5daac214be3121aaa47d7b2aab
Paola-dev26 <simoes.paola21@gmail.com>
2025-09-12 11:42:00 +0000

    tdb2

--- program.c
+++ program.c
@@ -2,29 +2,29 @@
 
 int main() {
    printf("+-----------------------------------------------------------------------+\n");
-   printf("|                                                                       |\n" );                                                                         
+   printf("|130                                                                    |\n" );                                                                         
    printf("|                                                                       |\n" );                                                       
    printf("|                                                                       |\n" );
    printf("|                                                                       |\n" );
+   printf("|120                                                                    |\n" );
    printf("|                                                                       |\n" );
    printf("|                                                                       |\n" );
    printf("|                                                                       |\n" );
+   printf("|110                                                                    |\n" );
+   printf("|                                                                       |\n" );
+   printf("|                                  |                                    |\n" );
+   printf("|                                  |                                    |\n" );
+   printf("|100                          -----------                               |\n" );
+   printf("|                                  |                                    |\n" );
+   printf("|                                  |                                    |\n" );
+   printf("|                                                                       |\n" );
+   printf("|090                                                                    |\n" );
    printf("|                                                                       |\n" );
    printf("|                                                                       |\n" );
    printf("|                                                                       |\n" );
+   printf("|080                                                                    |\n" );
    printf("|                                                                       |\n" );
    printf("|                                                                       |\n" );
-   printf("|                                                                       |\n" );
-   printf("|                                                                       |\n" );
-   printf("|                                                                       |\n" );
-   printf("|                                                                       |\n" );
-   printf("|                                                                       |\n" );
-   printf("|                                                                       |\n" );
-   printf("|                                                                       |\n" );
-   printf("|                                                                       |\n" );
-   printf("|                                                                       |\n" );
-   printf("|                                                                       |\n" );
-   printf("|                                                                       |\n" );
-   printf("|                                                                       |\n" );
+   printf("|km/h                                                                 m |\n" );
    printf("+-----------------------------------------------------------------------+\n");
 }
